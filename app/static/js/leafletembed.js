943ca061417aba5b9cee8626dbe4200fb2d744f6
Aymen Qader <aymen.qader@arm.com>
2018-07-17 19:12:17 +0100

    Added clusters

--- app/static/js/leafletembed.js
+++ app/static/js/leafletembed.js
@@ -4,13 +4,19 @@ function initmap()
 {
     var osmUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
     var osmAttrib='Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
-    var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 20, attribution: osmAttrib});
+    var osm = L.tileLayer(osmUrl, {minZoom: 1, maxZoom: 22, attribution: osmAttrib});
 
-    var schools = new L.GeoJSON.AJAX(school_file);
-    
-    map = new L.Map('map');
+    var latLng = L.latLng(4.624, -74.063)
+    map = L.map('map', {center: latLng, zoom: 13, maxZoom: 22, layers: [osm]});
+    var schools = L.geoJSON.ajax("static/data/schools.json",{
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup(feature.properties.name);
+        },
+    });
 
-    map.setView(new L.LatLng(4.624, -74.063), 8);
-    map.addLayer(osm);
-    map.addLayer(schools);
-};
+    var marker = L.markerClusterGroup({chunkedLoading: true});
+    schools.on('data:loaded', function(e){
+        marker.addLayer(schools);
+        map.addLayer(marker);
+    });
+}
