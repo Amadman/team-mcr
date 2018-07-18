07e6fc050c01113fa36e3f5c46056895b7008a3b
Aymen Qader <aymen.qader@arm.com>
2018-07-18 18:03:16 +0100

    Improved performance slightly

--- app/static/js/leafletembed.js
+++ app/static/js/leafletembed.js
@@ -7,16 +7,23 @@ function initmap()
     var osm = L.tileLayer(osmUrl, {minZoom: 1, maxZoom: 22, attribution: osmAttrib});
 
     var latLng = L.latLng(4.624, -74.063)
-    map = L.map('map', {center: latLng, zoom: 13, maxZoom: 22, layers: [osm]});
+    map = L.map('map', {center: latLng, zoom: 16, maxZoom: 22, layers: [osm]});
     var schools = L.geoJSON.ajax("static/data/schools.json",{
         onEachFeature: function(feature, layer) {
             layer.bindPopup(feature.properties.name);
         },
     });
 
-    var marker = L.markerClusterGroup({chunkedLoading: true});
+    var marker = L.markerClusterGroup({chunkedLoading: true, chunkSize: 200});
     schools.on('data:loaded', function(e){
-        marker.addLayer(schools);
+        async(function() {marker.addLayer(schools)}, null);
         map.addLayer(marker);
     });
+}
+
+function async(run_function, callback) {
+    setTimeout(function() {
+        run_function();
+        if (callback) {callback();}
+    }, 500);
 }
