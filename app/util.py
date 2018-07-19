dfd6af81fa93d7b0425f195d0051ef3b4d1e1342
Joseph Ellis <joseph.ellis@arm.com>
2018-07-19 17:05:40 +0100

    You should now prune before sorting for efficiency

--- app/util.py
+++ app/util.py
@@ -1,21 +1,24 @@
 
-def sort_data(schools):
-    """Sorts the jsonified list of schools/interest points first according to
-    ascending latitude, then according to ascending longnitude. Returns two
-    separate sorted lists.
-    """
-    features = schools["features"]
-    by_latitude = sorted(features, key=lambda e: e["geometry"]["coordinates"][0])
-    by_longnitude = sorted(features, key=lambda e: e["geometry"]["coordinates"][1])
-
-    return (by_latitude, by_longnitude)
-
 def prune_data(schools):
     """Prunes out all data except for id, latitude, and longnitude. Returns a
     json list with the following structure:
         [{"admin_id" : ..., "latitude" : ..., "longnitude" : ...}, ...]
     """
+    features = schools["features"]
     return [{"admin_id" : s["properties"]["admin_id"],
              "latitude" : s["geometry"]["coordinates"][0],
              "longnitude" : s["geometry"]["coordinates"][1],
-            } for s in schools]
+            } for s in features]
+
+def sort_data(pruned_schools):
+    """Sorts the jsonified list of schools/interest points first according to
+    ascending latitude, then according to ascending longnitude. Returns two
+    separate sorted lists.
+
+    Make sure that the data you pass to this method is pruned beforehand using
+    the prune_data method.
+    """
+    by_latitude = sorted(pruned_schools, key=lambda e: e["latitude"])
+    by_longnitude = sorted(pruned_schools, key=lambda e: e["longnitude"])
+
+    return (by_latitude, by_longnitude)
