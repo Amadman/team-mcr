b4681f456b57f1605c6098e9b47117ea3d4d521b
Joseph Ellis <joseph.ellis@arm.com>
2018-07-19 15:18:50 +0100

    Prettified download script output

--- app/static/data/pulldata.sh
+++ app/static/data/pulldata.sh
@@ -4,9 +4,16 @@ COLOMBIA_SHAPEFILE=https://data.humdata.org/dataset/50ea7fee-f9af-45a7-8a52-abb9
 COLOMBIA_ROADS_L=http://export.hotosm.org/downloads/36bc1e36-f6a4-4e33-a8b6-5c1b81b98ae5/hotosm_col_roads_lines_shp.zip
 COLOMBIA_ROADS_P=http://export.hotosm.org/downloads/36bc1e36-f6a4-4e33-a8b6-5c1b81b98ae5/hotosm_col_roads_polygons_shp.zip
 
-wget $COLOMBIA_SHAPEFILE -O colombia_shapefile.zip
-unzip colombia_shapefile.zip -d colombia_shapefile
-wget $COLOMBIA_ROADS_P -O colombia_roads_polygons.zip
-unzip colombia_roads_polygons.zip -d colombia_roads_polygons
-wget $COLOMBIA_ROADS_L -O colombia_roads_lines.zip
-unzip colombia_roads_lines.zip -d colombia_roads_lines
+WGET_OPTIONS="-q --show-progress"
+
+echo "Downloading data..."
+wget $WGET_OPTIONS $COLOMBIA_SHAPEFILE -O colombia_shapefile.zip
+wget $WGET_OPTIONS $COLOMBIA_ROADS_P -O colombia_roads_polygons.zip
+wget $WGET_OPTIONS $COLOMBIA_ROADS_L -O colombia_roads_lines.zip
+echo "Done!"
+
+echo "Inflating zipfiles..."
+unzip -q colombia_shapefile.zip -d colombia_shapefile
+unzip -q colombia_roads_polygons.zip -d colombia_roads_polygons
+unzip -q colombia_roads_lines.zip -d colombia_roads_lines
+echo "Done!"
