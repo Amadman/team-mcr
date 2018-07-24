147f811a51c0da57630c52ef79dc68e428d895d7
Amad Aslam <amad.aslam@arm.com>
2018-07-24 13:57:40 +0100

    Add Websocket
    Websocket connection between the Flask server and the Client.
    Moving the slider sends the new 'x' value to the server
    which processes and sends updated JSON points to the Client Map.

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,9 @@ import logging
 import os
 
 from flask import Flask
+from flask_socketio import SocketIO
 from config import Config
+import json
 
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 log = logging.getLogger(__name__)
@@ -12,6 +14,7 @@ __version__ = "0.0.1"
 
 app = Flask(__name__)
 app.config.from_object(Config)
+socketio = SocketIO(app)
 
 # Stop the jinja2 templating from putting in unnecessary whitespace.
 app.jinja_env.trim_blocks = True
@@ -22,6 +25,20 @@ def setup_webapp():
     log.info("Starting webapp...")
     log.info("No startup tasks required at this early stage.")
 
+@socketio.on('event')
+def on_connect(message):
+  print('received message: {0}'.format(str(message)))
+
+@socketio.on('update')
+def update_x(message):
+  print('update: {0}'.format(str(message)))
+  with open(os.path.join(app.root_path, 'static', 'data', 'new_schools.json')) as f:
+    data = json.load(f)
+  socketio.emit('data', data)
+
 setup_webapp()
 
 from app import routes, errors
+
+if __name__ == '__main__':
+  socketio.run(app)
--- app/static/js/leafletembed.js
+++ app/static/js/leafletembed.js
@@ -1,18 +1,29 @@
 var map;
+var socket;
 
 function initmap()
 {
+    socket = io.connect('http://' + document.domain + ':' + location.port);
+
     var osmUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
     var osmAttrib='Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
     var osm = L.tileLayer(osmUrl, {minZoom: 1, maxZoom: 22, attribution: osmAttrib});
 
     var latLng = L.latLng(4.624, -74.063)
-    map = L.map('map', {center: latLng, zoom: 16, maxZoom: 22, layers: [osm]});
-    var schools = L.geoJSON.ajax("static/data/schools.json",{
-        onEachFeature: function(feature, layer) {
-            layer.bindPopup(feature.properties.name);
-        },
-    });
+    map = L.map('map', {center: latLng, zoom: 13, maxZoom: 22, layers: [osm]}),
+        realtime = L.realtime(undefined, {
+            getFeatureId: function(f) { return f.geometry.coordinates[0]+f.geometry.coordinates[1]; },
+            start: false
+        }).addTo(map);
+
+    function update(e) {
+        console.log("Got an update!")
+        realtime.update(e);
+    }
+
+    function remove(e) {
+        realtime.remove(JSON.parse(e.data));
+    }
 
     var health = L.shapefile("static/data/healthsites.zip", {
         pointToLayer: function (feature, latlng) {
@@ -28,15 +39,56 @@ function initmap()
             }
         }
     });
+
     var healthCluster = L.markerClusterGroup({chunkedLoading: true});
     var marker = L.markerClusterGroup({chunkedLoading: true, chunkSize: 200});
     health.on('data:loaded', function(e){
         healthCluster.addLayer(health);
         map.addLayer(healthCluster);
     })
+
+    /*
+    var schools = L.geoJSON.ajax("static/data/schools.json",{
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup(feature.properties.name);
+        },
+    });
+
+    var marker = L.markerClusterGroup({chunkedLoading: true});
     schools.on('data:loaded', function(e){
         async(function() {marker.addLayer(schools)}, null);
         map.addLayer(marker);
+    });
+    */
+
+    socket.on('connect', function() {
+        socket.emit('event', {data: 'Connected!'});
+    });
+    socket.on('data', function(e) {
+        update(e);
+    });
+
+    var marker = L.markerClusterGroup({chunkedLoading: true});
+    realtime.on('update', function(e) {
+      var popupContent = function(fId) {
+          var feature = e.features[fId];
+          return feature.properties.name
+        },
+        bindFeaturePopup = function(fId) {
+          realtime.getLayer(fId).bindPopup(popupContent(fId));
+        },
+        updateFeaturePopup = function(fId) {
+          realtime.getLayer(fId).getPopup().setContent(popupContent(fId));
+        },
+        bindMarker = function(fId) {
+          marker.addLayer(realtime.getLayer(fId));
+        };
+
+      Object.keys(e.enter).forEach(bindFeaturePopup);
+      Object.keys(e.enter).forEach(bindMarker);
+      Object.keys(e.update).forEach(updateFeaturePopup);
+
+      map.addLayer(marker);
     });
 }
 
