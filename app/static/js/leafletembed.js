cadfd174efe35e27acd5c0e600ecdb87a8f7c768
Aymen Qader <aymen.qader@arm.com>
2018-07-25 14:24:08 +0100

    Cluster on data update

--- app/static/js/leafletembed.js
+++ app/static/js/leafletembed.js
@@ -8,13 +8,9 @@ function initmap()
     var osmUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
     var osmAttrib='Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
     var osm = L.tileLayer(osmUrl, {minZoom: 1, maxZoom: 22, attribution: osmAttrib});
+    var realtime;
 
     var latLng = L.latLng(4.624, -74.063)
-    map = L.map('map', {center: latLng, zoom: 13, maxZoom: 22, layers: [osm]}),
-        realtime = L.realtime(undefined, {
-            getFeatureId: function(f) { return f.geometry.coordinates[0]+f.geometry.coordinates[1]; },
-            start: false
-        }).addTo(map);
 
     function update(e) {
         console.log("Got an update!")
@@ -27,7 +23,7 @@ function initmap()
 
     var health = L.shapefile("static/data/healthsites.zip", {
         pointToLayer: function (feature, latlng) {
-           return L.circleMarker(latlng, {radius: 20, fillOpacity: 0.8, fillColor: "#ff0000"}) 
+            return L.circleMarker(latlng, {radius: 20, fillOpacity: 0.8, fillColor: "#ff0000"})
         },
         onEachFeature: function(feature, layer) {
             if (feature.properties) {
@@ -44,8 +40,38 @@ function initmap()
     var marker = L.markerClusterGroup({chunkedLoading: true, chunkSize: 200});
     health.on('data:loaded', function(e){
         healthCluster.addLayer(health);
+        map = L.map('map', {center: latLng, zoom: 13, maxZoom: 22, layers: [osm]}),
+            realtime = L.realtime(undefined, {
+                container: marker,
+                getFeatureId: function(f) { return f.geometry.coordinates[0]+f.geometry.coordinates[1]; },
+                start: false
+            }).addTo(map);
         map.addLayer(healthCluster);
+        realtime.on('update', function(e) {
+
+            marker = L.markerClusterGroup({chunkedLoading: true});
+            var popupContent = function(fId) {
+                var feature = e.features[fId];
+                return feature.properties.name
+            };
+            bindFeaturePopup = function(fId) {
+                realtime.getLayer(fId).bindPopup(popupContent(fId));
+            },
+                updateFeaturePopup = function(fId) {
+                    realtime.getLayer(fId).getPopup().setContent(popupContent(fId));
+                },
+                bindMarker = function(fId) {
+                    marker.addLayer(realtime.getLayer(fId));
+                };
+
+            Object.keys(e.enter).forEach(bindFeaturePopup);
+            Object.keys(e.enter).forEach(bindMarker);
+            Object.keys(e.update).forEach(updateFeaturePopup);
+
+            map.addLayer(marker);
+        });
     })
+
 
     /*
     var schools = L.geoJSON.ajax("static/data/schools.json",{
@@ -68,28 +94,6 @@ function initmap()
         update(e);
     });
 
-    var marker = L.markerClusterGroup({chunkedLoading: true});
-    realtime.on('update', function(e) {
-      var popupContent = function(fId) {
-          var feature = e.features[fId];
-          return feature.properties.name
-        },
-        bindFeaturePopup = function(fId) {
-          realtime.getLayer(fId).bindPopup(popupContent(fId));
-        },
-        updateFeaturePopup = function(fId) {
-          realtime.getLayer(fId).getPopup().setContent(popupContent(fId));
-        },
-        bindMarker = function(fId) {
-          marker.addLayer(realtime.getLayer(fId));
-        };
-
-      Object.keys(e.enter).forEach(bindFeaturePopup);
-      Object.keys(e.enter).forEach(bindMarker);
-      Object.keys(e.update).forEach(updateFeaturePopup);
-
-      map.addLayer(marker);
-    });
 }
 
 function async(run_function, callback) {
